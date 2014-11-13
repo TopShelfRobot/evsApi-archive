56be2d454c5cf3fc39faa20bad9212c75be63d90
boone <boone.mike@gmail.com>
2014-11-13 10:18:28 -0500

    add new proj for auth

---
+++ evs.API/AuthContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using evs.Model;
+
+namespace evs.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("evsContext")
+        {
+
+        }
+
+        //public DbSet<Client> Clients { get; set; }
+        //public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
--- evs.Model/AccountModels.cs
+++ evs.Model/AccountModels.cs
@@ -18,6 +18,24 @@ namespace evs.Model
     //    public DbSet<UserProfile> UserProfiles { get; set; }
     //}
 
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
     [Table("UserProfile")]
     public class UserProfile
     {
